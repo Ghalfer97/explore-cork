1b5b313705efadf4530c941c141819776b5ef6f9
Ghalfer97 <fearghalorourke@hotmail.com>
2025-05-09 13:17:33 +0100

    Test pubs classes

--- pubs_in_cork.py
+++ pubs_in_cork.py
@@ -23,72 +23,88 @@ from selenium.webdriver.support import expected_conditions as EC
 #soup=BeautifulSoup(html_content,"html.parser")
 #pubs=soup.find_all("div",class_="section-result-details-container")
 
-results=set()
-delay = 10
-chrome_options = webdriver.ChromeOptions()
-#chrome_options.add_argument('--headless')
-#chrome_options.add_argument('start-maximized')
-chrome_options.add_argument('disable-infobars')
-chrome_options.add_experimental_option("useAutomationExtension", False)
-chrome_options.add_experimental_option("excludeSwitches",["enable-automation"])
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
-#WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.optanon-allow-all.accept-cookies-button"))).click()
-#exit()
+class WebDriver:
 
-if 1==1:
-    #@51.8945394,-8.4942599,
-    list_of_possible_keywords=['cocktail','bars','pubs','gastropubs'] #'restaurant'
-    i=0
-    for z in range(1,5):
-        x=random.uniform(51.84,51.9)
-        y=random.uniform(-8.44,-8.5)
-        for i in range(0,4):
-            search_area=f"https://www.google.com/maps/search/{list_of_possible_keywords[i]}+cork/@{x},{y},14z/data=!4m2!2m1!6e5?entry=ttu"
-            i+=1
-            #print(i)
-            driver.get(search_area)
-            time.sleep(10)
-            message = driver.find_elements(by=By.CSS_SELECTOR, value="div")
-            #message = driver.find_elements(by=By.CSS_SELECTOR, value="button") #Found The Shelbourne bar
-            #for a_message in message:
-            #	print (a_message.text) #Found address of shelbourne, style of pub, delivery,etc
-            #message[200].find_element(By.CSS_SELECTOR, "div.fontHeadlineSmall").text #Found shelbourne bar
+    def __init__(self):
+    #if 1==1:
+        self.results=set()
+        self.delay = 10
+        self.chrome_options = webdriver.ChromeOptions()
+        #chrome_options.add_argument('--headless')
+        #chrome_options.add_argument('start-maximized')
+        self.chrome_options.add_argument('disable-infobars')
+        self.chrome_options.add_experimental_option("useAutomationExtension", False)
+        self.chrome_options.add_experimental_option("excludeSwitches",["enable-automation"])
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=self.chrome_options)
+        self.output_location="output_files/test_pubs_in_cork.csv"
+        #WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.optanon-allow-all.accept-cookies-button"))).click()
+        #exit()
 
-            #driver.close()
-            index_number=0
-            for a_message in message:
-                try:
-                    pub_name=a_message.find_element(By.CSS_SELECTOR, "div.fontHeadlineSmall")
-                    results.add(str(pub_name.text))
-                    #print(pub_name.text,index_number)
-                    index_number+=1
-                    avg_rating = a_message.find_element(By.CLASS_NAME,"section-star-display")
-                    total_reviews = a_message.find_element(By.CLASS_NAME,"section-rating-term")
-                    address = a_message.find_element(By.CSS_SELECTOR,"[data-item-id='address']")
-                    phone_number = a_message.find_element(By.CSS_SELECTOR,"[data-tooltip='Copy phone number']")
-                    website = a_message.find_element(By.CSS_SELECTOR,"[data-item-id='authority']")
-                except:
-                    pass
-                #try:
-                #    results.add(str(pub_name.text))#+';'+str(avg_rating.text))
-                #    print(pub_name.text,index_number)
-                #except:
-                #    pass
-    
+    def get_pub_information(self,message):
+        index_number=0
+        for a_message in message:
+            try:
+                pub_name=a_message.find_element(By.CSS_SELECTOR, "div.fontHeadlineSmall")
+                self.results.add(str(pub_name.text))
+                #print(pub_name.text,index_number)
+                index_number+=1
+                avg_rating = a_message.find_element(By.CLASS_NAME,"section-star-display")
+                total_reviews = a_message.find_element(By.CLASS_NAME,"section-rating-term")
+                address = a_message.find_element(By.CSS_SELECTOR,"[data-item-id='address']")
+                phone_number = a_message.find_element(By.CSS_SELECTOR,"[data-tooltip='Copy phone number']")
+                website = a_message.find_element(By.CSS_SELECTOR,"[data-item-id='authority']")
+            except:
+                pass
+            #try:
+            #    results.add(str(pub_name.text))#+';'+str(avg_rating.text))
+            #    print(pub_name.text,index_number)
+            #except:
+            #    pass
+        return self.results
 
-    madelyns_pubs = ['Sin é','The Corner House','Mutton Lane Inn',"Arthur Mayne's Pharmacy","Fionnbarra","The Long Valley Bar","The Raven Bar","Vicarstown Bar","The Friary","BarBarossa OliverPlunkett Street",
-                     "Barbarella","Tom Barry's","Mr Bradley's","The Abbey Tavern","The Pav","Crack Jenny's","Paddy the Farmers","Coughlans Bar","Crane Lane","An Bróg Bar + Kitchen","Brick Lane",
-                     "Conway's Yard","Abbott's Ale House","Maureen's","The Shelbourne Bar","The Oliver Plunkett","Fred Zeppelins","Old Oak","Clancy's Cork","The Oval","The Park","Spailpin Fanach",
-                     "O'Sho","Rearden's Bar","Dwyers of Cork","Costigan's Pub","The Courtyard on Sober Lane","Rosie Maddison's","The Roundy","Deep South","Perch - Rooftop at SoHo","Gaia",
-                     "Impala","Chambers","The Welcome Inn","The Poor Relation","Cask","The Mardyke Entertainment Complex","Fordes Bar","The Beer Garden","The Woodford","Franciscan Well Brewery & Brewpub",
-                     "The Hyde Out","Bru Bar","Paladar","Rising Sons Brewery","The Rob Roy","Gables","Cissie Youngs","Annie Mac's","Bierhaus","An Sibín","Upstairs"]
-    fearghals_pubs=['Sin é','The Corner House','Mutton Lane Inn',"Arthur Mayne's Pharmacy","Fionnbarra","The Long Valley Bar","The Raven Bar","Vicarstown Bar","The Friary","BarBarossa OliverPlunkett Street",
-                    "Barbarella","Tom Barry's","Mr Bradley's","The Pav","Crack Jenny's","Coughlans Bar","Crane Lane","An Bróg Bar + Kitchen","Conway's Yard","Abbott's Ale House",
-                    "The Shelbourne Bar","The Oliver Plunkett","Fred Zeppelins","Old Oak","Clancy's Cork","The Oval","Spailpin Fanach","O'Sho","Rearden's Bar","Dwyers of Cork","Costigan's Pub",
-                    "The Courtyard on Sober Lane","Rosie Maddison's","The Roundy","Deep South","Perch - Rooftop at SoHo","Gaia","Impala","Chambers","The Welcome Inn","The Poor Relation","Cask",
-                    "The Mardyke Entertainment Complex","Fordes Bar","The Beer Garden","The Woodford","Franciscan Well Brewery & Brewpub","The Hyde Out","Paladar","Rising Sons Brewery","The Rob Roy","Gables",
-                    "Cissie Youngs","Annie Mac's","Bierhaus","The Hi-B Bar","An Sibín","Ryan's Bar","Canty's Bar","Barcadia Video Arcade","Upstairs"]
-    rows=[]
-    rows.extend(results)
-    pubs_df=pd.DataFrame({'Pub_Name':rows})
-    pubs_df.to_csv("output_files/test_pubs_in_cork.csv",index=False)
+    def get_location_data(self):
+        #@51.8945394,-8.4942599,
+        list_of_possible_keywords=['cocktail','bars','pubs','gastropubs'] #'restaurant'
+        i=0
+        for z in range(1,4):
+            x=random.uniform(51.84,51.9)
+            y=random.uniform(-8.44,-8.5)
+            for i in range(0,4):
+                search_area=f"https://www.google.com/maps/search/{list_of_possible_keywords[i]}+cork/@{x},{y},14z/data=!4m2!2m1!6e5?entry=ttu"
+                i+=1
+                #print(i)
+                self.driver.get(search_area)
+                time.sleep(10)
+                message = self.driver.find_elements(by=By.CSS_SELECTOR, value="div")
+                #message = driver.find_elements(by=By.CSS_SELECTOR, value="button") #Found The Shelbourne bar
+                #for a_message in message:
+                #	print (a_message.text) #Found address of shelbourne, style of pub, delivery,etc
+                #message[200].find_element(By.CSS_SELECTOR, "div.fontHeadlineSmall").text #Found shelbourne bar
+                #self.driver.close()
+                self.get_pub_information(message)
+        self.driver.quit()
+
+        return(self.results)
+        
+    def output_file(self):
+        self.get_location_data()
+        self.madelyns_pubs = ['Sin é','The Corner House','Mutton Lane Inn',"Arthur Mayne's Pharmacy","Fionnbarra","The Long Valley Bar","The Raven Bar","Vicarstown Bar","The Friary","BarBarossa OliverPlunkett Street",
+                        "Barbarella","Tom Barry's","Mr Bradley's","The Abbey Tavern","The Pav","Crack Jenny's","Paddy the Farmers","Coughlans Bar","Crane Lane","An Bróg Bar + Kitchen","Brick Lane",
+                        "Conway's Yard","Abbott's Ale House","Maureen's","The Shelbourne Bar","The Oliver Plunkett","Fred Zeppelins","Old Oak","Clancy's Cork","The Oval","The Park","Spailpin Fanach",
+                        "O'Sho","Rearden's Bar","Dwyers of Cork","Costigan's Pub","The Courtyard on Sober Lane","Rosie Maddison's","The Roundy","Deep South","Perch - Rooftop at SoHo","Gaia",
+                        "Impala","Chambers","The Welcome Inn","The Poor Relation","Cask","The Mardyke Entertainment Complex","Fordes Bar","The Beer Garden","The Woodford","Franciscan Well Brewery & Brewpub",
+                        "The Hyde Out","Bru Bar","Paladar","Rising Sons Brewery","The Rob Roy","Gables","Cissie Youngs","Annie Mac's","Bierhaus","An Sibín","Upstairs"]
+        self.fearghals_pubs=['Sin é','The Corner House','Mutton Lane Inn',"Arthur Mayne's Pharmacy","Fionnbarra","The Long Valley Bar","The Raven Bar","Vicarstown Bar","The Friary","BarBarossa OliverPlunkett Street",
+                        "Barbarella","Tom Barry's","Mr Bradley's","The Pav","Crack Jenny's","Coughlans Bar","Crane Lane","An Bróg Bar + Kitchen","Conway's Yard","Abbott's Ale House",
+                        "The Shelbourne Bar","The Oliver Plunkett","Fred Zeppelins","Old Oak","Clancy's Cork","The Oval","Spailpin Fanach","O'Sho","Rearden's Bar","Dwyers of Cork","Costigan's Pub",
+                        "The Courtyard on Sober Lane","Rosie Maddison's","The Roundy","Deep South","Perch - Rooftop at SoHo","Gaia","Impala","Chambers","The Welcome Inn","The Poor Relation","Cask",
+                        "The Mardyke Entertainment Complex","Fordes Bar","The Beer Garden","The Woodford","Franciscan Well Brewery & Brewpub","The Hyde Out","Paladar","Rising Sons Brewery","The Rob Roy","Gables",
+                        "Cissie Youngs","Annie Mac's","Bierhaus","The Hi-B Bar","An Sibín","Ryan's Bar","Canty's Bar","Barcadia Video Arcade","Upstairs"]
+        self.rows=[]
+        self.rows.extend(self.results)
+        self.pubs_df=pd.DataFrame({'Pub_Name':self.rows})
+        self.pubs_df.to_csv(self.output_location,index=False)
+        return (self.results)
+
+x = WebDriver()
+results=x.output_file()
